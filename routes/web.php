cccaa266ab5e84213c1b816922ea1c0829cf918f
Alex Anufriev <tekerrr@list.ru>
2019-11-06 19:04:45 +1000

    Reformated some code in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
-Route::get('/', function () {
-    return redirect('/posts');
-});
+Route::redirect('/', '/posts');
 
 Route::resource('posts', 'PostController');
 
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contacts', function () {
-    return view('contacts');
-});
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::view('/about', 'about');
+Route::view('/contacts', 'contacts');
+Route::view('/admin', 'admin.index');
 
 Route::get('/admin/feedback', 'FeedbackController@index');
 Route::post('/admin/feedback', 'FeedbackController@store');
