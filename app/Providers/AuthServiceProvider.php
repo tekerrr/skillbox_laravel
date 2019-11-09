9356e772961c856750724451f22f634448d7d02f
Alex Anufriev <tekerrr@list.ru>
2019-11-09 16:34:48 +1000

    Granted Admin rights

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -20,12 +20,17 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      *
+     * @param \Illuminate\Contracts\Auth\Access\Gate $gate
      * @return void
      */
-    public function boot()
+    public function boot(\Illuminate\Contracts\Auth\Access\Gate $gate)
     {
         $this->registerPolicies();
 
-        //
+        $gate->before(function ($user) {
+            if ($user->id == config('admin.id')) {
+                return true;
+            }
+        });
     }
 }
