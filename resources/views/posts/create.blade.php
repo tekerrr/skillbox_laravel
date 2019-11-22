db9ee92eb1587e9e526e00d5cefddeca9e758da8
Alex Anufriev <tekerrr@list.ru>
2019-11-22 14:07:05 +1000

    Fixed post.created/update Views

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -41,7 +41,7 @@
         <div class="form-group form-check">
             <input class="form-check-input" type="checkbox" id="checkboxActive" value="1"
                    name="is_active" {{ old('is_active') ? 'checked' : '' }}>
-            <label class="form-check-label" for="checkboxActive">Опубликовать?</label>
+            <label class="form-check-label" for="checkboxActive">Опубликовать</label>
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Создать статью</button>
