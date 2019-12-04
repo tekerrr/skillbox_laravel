069b800877443f015a7b91b926a669f6fa933a64
Alex Anufriev <tekerrr@list.ru>
2019-12-05 09:08:39 +1000

    Updated history view

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -1,7 +1,10 @@
 <hr>
 <h5>История изменений</h5>
 @forelse ($history as $author)
-    <p>{{ $author->name }} - {{ $author->pivot->created_at->diffForHumans() }} = {{ $author->pivot->before }} - {{ $author->pivot->after }}</p>
+    <h6>{{ $author->name }}</h6>
+    <p class="blog-post-meta">{{ $author->pivot->created_at->diffForHumans() }}</p>
+    <p>− {{ $author->pivot->before }}</p>
+    <p>+ {{ $author->pivot->after }}</p>
 @empty
     <p>Нет истории изменения</p>
 @endforelse
