4cd63ff24bd70c658fac18f83f62743c9f51c80e
Alex Anufriev <tekerrr@list.ru>
2019-12-05 19:40:27 +1000

    Fixed class name

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -8,6 +8,6 @@ class StatisticsController extends Controller
 {
     public function index()
     {
-        return view('admin.statistics', ['data' => (new \App\Statisctics())->get()]);
+        return view('admin.statistics', ['data' => (new \App\Statistics())->get()]);
     }
 }
--- app/Statistics.php
+++ app/Statistics.php
@@ -4,7 +4,7 @@
 namespace App;
 
 
-class Statisctics
+class Statistics
 {
     private $data = [];
 
